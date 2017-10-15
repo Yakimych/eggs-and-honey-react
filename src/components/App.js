09c5bdad63c5580f11e929faad36f7a30b703bb9
Kyrylo Yakymenko <kyrylo.yakymenko@antura.se>
2017-10-15 15:41:56 +0200

    Move the service call and order list handling logic into the OrderListContainer, so that the whole component doesn't get reloaded every time an order is added

--- src/components/App.js
+++ src/components/App.js
@@ -2,32 +2,11 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 
-import OrderService from '../services/OrderService';
 import OrderListContainer from './OrderListContainer';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { orders: [] };
-  }
-
-  componentDidMount() {
-    this.getOrders();
-  }
-
-  getOrders = () => {
-    OrderService.getOrders()
-      .then(orders => this.setState({ orders: orders }))
-      .catch(error => { console.log(error); });
-  }
-  
-  onAddOrder = (name, order) => {
-    OrderService.addOrder(name, order).then(() => {
-      this.setState((prevState) => {
-        let newOrders = prevState.orders.concat({ name: name, order: order}); 
-        return { orders: newOrders };
-      });
-    });
   }
 
   render() {
@@ -38,8 +17,8 @@ class App extends Component {
         </div>
         <BrowserRouter>
           <div className="main-content">
-            <Route path="/" exact component={(props) => (<OrderListContainer {...props} orders={this.state.orders} onAddOrder={this.onAddOrder} />)} />
-            <Route path="/admin" component={(props) => (<h1><OrderListContainer {...props} orders={this.state.orders} /></h1>)} />
+            <Route path="/" exact component={(props) => (<OrderListContainer {...props} />)} />
+            <Route path="/admin" component={(props) => (<h1><OrderListContainer {...props} /></h1>)} />
           </div>
         </BrowserRouter>
       </div>
--- src/components/OrderListContainer.js
+++ src/components/OrderListContainer.js
@@ -1,41 +1,59 @@
 import React from 'react';
 import OrderList from './OrderList';
 import AddOrder from './AddOrder';
+import OrderService from '../services/OrderService';
 
 class OrderListContainer extends React.Component {
+  orders = [];
+
   constructor(props) {
     super(props);
-    // TODO: Fetch orders from the service here
     this.state = {
-      orders: props.orders,
+      filteredOrders: [],
       productTypes: [ "Eggs", "Honey" ]
     };
   }
 
   componentDidMount() {
-    this.filterOrders(-1);
+    this.getOrders();
   }
 
-  filterOrders = (productIndex) => {
+  getOrders = () => {
+    OrderService.getOrders()
+      .then(orders => {
+        this.orders = orders;
+        this.updateFilteredOrders(-1);
+      })
+      .catch(error => { console.log(error); });
+  }
+
+ onAddOrder = (name, order) => {
+    OrderService.addOrder(name, order).then(() => {
+      this.orders = this.orders.concat({ name: name, order: order}); 
+      this.updateFilteredOrders(this.state.productTypes.indexOf(order));
+    });
+  }
+ 
+  updateFilteredOrders = (productIndex) => {
     const selectedProductName = this.state.productTypes[productIndex];
     
     let filteredOrders =
       productIndex === -1 ?
-      this.props.orders :
-      this.props.orders.filter((order) =>
-        order.order === selectedProductName);
+        this.orders :
+        this.orders.filter((order) =>
+          order.order === selectedProductName);
 
-    this.setState({ orders: filteredOrders });
+    this.setState({ filteredOrders: filteredOrders });
   }
 
   render() {
     return (
      <div>
-       <OrderList orders={this.state.orders} />
+       <OrderList orders={this.state.filteredOrders} />
        <AddOrder
-         onAddOrder={this.props.onAddOrder}
+         onAddOrder={this.onAddOrder}
          products={this.state.productTypes}
-         activeProductChanged={this.filterOrders}
+         activeProductChanged={this.updateFilteredOrders}
        />
      </div>
     );
