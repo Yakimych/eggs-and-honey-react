4c327dbc8316fe04fb7f5d22e2e626c21de98990
Kyrylo Yakymenko <yakimych@gmail.com>
2018-05-13 14:06:21 +0200

    Add return types to promises in DataProvider

--- src/services/DataProvider.js
+++ src/services/DataProvider.js
@@ -1,28 +1,28 @@
 // @flow
-import type { OrderItems, ResolvedOrderItems, ApiResult, ResultWithId } from '../Types/OrderTypes';
+import type { Order, ResolvedOrder, OrderItems, ResolvedOrderItems, ApiResult, ResultWithId } from '../Types/OrderTypes';
 import axios from 'axios';
 
-const apiUrl = 'http://eggsandhoneywebapiinmemory.azurewebsites.net/api/v1/';
+const apiUrl = 'http://localhost:5000/api/v1/';
 const getOrdersUrl = `${apiUrl}orders`;
 const getResolvedOrdersUrl = `${apiUrl}resolvedorders`;
-const addOrderUrl = `${apiUrl}orders/add`;
+const addOrderUrl = `${apiUrl}orders`;
 const resolveOrderUrl = `${apiUrl}orders/resolve`;
 const unresolveOrderUrl = `${apiUrl}resolvedorders/unresolve`;
 
 class DataProvider {
-  getOrders() {
+  getOrders(): Promise<OrderItems> {
     return axios.get(getOrdersUrl).then((result: ApiResult<OrderItems>) => result.data);
   }
-  getResolvedOrders() {
+  getResolvedOrders(): Promise<ResolvedOrderItems> {
     return axios.get(getResolvedOrdersUrl).then((result: ApiResult<ResolvedOrderItems>) => result.data);
   }
-  addOrder(name: string, order: string) {
+  addOrder(name: string, order: string): Promise<ResultWithId> {
     return axios.post(addOrderUrl, { name: name, order: order }).then((result: ApiResult<ResultWithId>) => result.data.id);
   }
-  resolveOrder(orderId: number) {
+  resolveOrder(orderId: number): Promise<ResolvedOrder> {
     return axios.post(resolveOrderUrl, { id: orderId }).then((result: ApiResult<ResultWithId>) => result.data);
   }
-  unresolveOrder(orderId: number) {
+  unresolveOrder(orderId: number): Promise<Order> {
     return axios.post(unresolveOrderUrl, { id: orderId }).then((result: ApiResult<ResultWithId>) => result.data);
   }
 }
